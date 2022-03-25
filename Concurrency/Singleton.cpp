a43ad5ab08e1ff06ec162fb3ff6f153490b85098
Caturra <wasdjkl233@gmail.com>
2022-03-26 00:11:41 +0800

    update singleton: use relaxed

--- Concurrency/Singleton.cpp
+++ Concurrency/Singleton.cpp
@@ -5,6 +5,9 @@ class Singleton {
 public:
     static Singleton* instance() {
         // 可以进一步放宽使用consume
+        // 但是肯定不能用relaxed
+        // 为了满足后面ptr = new Singleton();并release的happens-before关系（可能由其它线程操作）
+        // 你需要acquire-release或者consume-release
         auto ptr = _instance.load(std::memory_order_acquire);
         if(!ptr) {
             std::lock_guard<std::mutex> _ {_mutex};
@@ -16,7 +19,11 @@ public:
             // Question. 在上锁的前提这里能不能直接用relaxed？
             // 似乎不行，因为instance()一开始的load就是在锁外部的
             // 但是这种属于“有关联”的program order，CPU也会乱序执行吗？
-            ptr = _instance.load(std::memory_order_acquire);
+            // ptr = _instance.load(std::memory_order_acquire);
+            //
+            // 根据https://www.zhihu.com/question/351434327/answer/883758362
+            // 上了锁就能保证可见性了
+            ptr = _instance.load(std::memory_order_relaxed);
             if(!ptr) {
                 ptr = new Singleton();
                 _instance.store(ptr, std::memory_order_release);
