00bc55ad7e20538db0ab4091f5eece12e895844e
Caturra <wasdjkl233@gmail.com>
2022-05-23 13:26:26 +0800

    update shared mutex

--- Concurrency/SharedMutex.cpp
+++ Concurrency/SharedMutex.cpp
@@ -12,49 +12,41 @@ private:
     // n == 0：未上锁
     // n == -1：被一个writer持有
     // n > 0：被n个reader持有
-    // 注意初始化，老是忘
     std::atomic<int> _n {0};
 };
 
 inline void SharedMutex::lockShared() {
-    int old;
-    do {
-        old = _n.load(std::memory_order_relaxed);
+    for(;;) {
+        int old = _n.load(std::memory_order_relaxed);
         if(old < 0) continue;
-    // 这里如果失败的话其实old应该也会得到一个更新值（expected = *this），没必要下一次old = ...，
-    // 但是仍要考虑首次old < 0的情况，因此还是决定写繁琐点
-    //
-    // 内存模型上CAS如果成功那就是RMW，失败则load（为了满足expected得到实际值）
-    // 见：https://en.cppreference.com/w/cpp/atomic/atomic/compare_exchange
-    // 如果只论x86的话，memory_order怎么写都是对的，因为要成功必须RMW，RMW必然有类似LOCK前缀的保证
-    // 目前只是简单的遵循acquire-release，后面看看能不能直接仅一个fence就足够了？
-    } while(!_n.compare_exchange_strong(old, old+1, std::memory_order_acquire, std::memory_order_relaxed));
+        if(_n.compare_exchange_weak(old, old+1,
+            std::memory_order_acquire, std::memory_order_relaxed)) return;
+    }
 }
 
 inline void SharedMutex::unlockShared() {
-    int old;
-    do {
-        old = _n.load(std::memory_order_relaxed);
+    for(;;) {
+        int old = _n.load(std::memory_order_relaxed);
         if(old <= 0) continue;
-    } while(!_n.compare_exchange_strong(old, old-1, std::memory_order_release, std::memory_order_relaxed));
+        if(_n.compare_exchange_weak(old, old-1,
+            std::memory_order_release, std::memory_order_relaxed)) return;
+    }
 }
 
 inline void SharedMutex::lock() {
-    int old;
-    do {
-        old = 0;
-        // old = _n.load(std::memory_order_relaxed);
-        // if(old != 0) continue;
-    } while(!_n.compare_exchange_strong(old, -1, std::memory_order_acquire, std::memory_order_relaxed));
+    for(;;) {
+        int old = 0;
+        if(_n.compare_exchange_strong(old, -1,
+            std::memory_order_acquire, std::memory_order_relaxed)) return;
+    }
 }
 
 inline void SharedMutex::unlock() {
-    int old;
-    do {
-        old = -1;
-        // old = _n.load(std::memory_order_relaxed);
-        // if(old != -1) continue;
-    } while(!_n.compare_exchange_strong(old, 0, std::memory_order_release, std::memory_order_relaxed));
+    for(;;) {
+        int old = -1;
+        if(_n.compare_exchange_strong(old, 0,
+            std::memory_order_release, std::memory_order_relaxed)) return;
+    }
 }
 
 int main() {
