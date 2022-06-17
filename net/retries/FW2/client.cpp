08f62e597e555920f5559440c308a4baef4c1a5f
Caturra <wasdjkl233@gmail.com>
2022-06-18 02:14:30 +0800

    update FW2: add forced mode

--- net/retries/FW2/client.cpp
+++ net/retries/FW2/client.cpp
@@ -1,13 +1,28 @@
 #include <bits/stdc++.h>
 #include "fluent.hpp"
 
-int main() {
+// 0: shutdown
+// 1: close
+int main(int argc, const char *argv[]) {
+
+    bool forced = false;
+    if(argc <= 2 && argv[1][0] == '1') {
+        forced = true;
+        std::cout << "enable forced mode" << std::endl;
+    } else {
+        std::cout << "disable forced mode" << std::endl;
+    }
+
     fluent::InetAddress address {INADDR_ANY, 2565};
     fluent::Client client;
 
-    client.connect(address, [](fluent::Context *context) {
-        // send FIN
-        context->shutdown();
+    client.connect(address, [forced](fluent::Context *context) {
+        if(forced) {
+            ::close(context->socket.fd());
+            context->socket.detach();
+        } else {
+            context->shutdown();
+        }
         return "404notfound";
     });
 
