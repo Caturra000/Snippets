9ff74be1829fc45c327419ec2a485cad329cb36f
Caturra <wasdjkl233@gmail.com>
2022-11-16 06:09:59 +0800

    update binding

--- Linkage/symbol/binding/binding1.cpp
+++ Linkage/symbol/binding/binding1.cpp
@@ -1,10 +1,12 @@
-// 如果生成.o，那么全部binding方式都是LOCAL（或者不存在这个符号）
 // $ g++ -c binding1.cpp --std=c++17 -o binding1.o && readelf --symbols binding1.o | c++filt > dump
 
 extern int a;
 
+// OBJECT  LOCAL  DEFAULT
 static int b0;
+// OBJECT  LOCAL  DEFAULT
 static int b1 = 0;
+// OBJECT  LOCAL  DEFAULT
 static int b2 = 1;
 
 // -stdc++=17
@@ -12,11 +14,9 @@ inline int c0;
 inline int c1 = 0;
 inline int c2 = 2;
 
-extern int funca(double);
-
-static int funcb0(double);
-// FUNC    LOCAL  DEFAULT
-static int funcb1(double) {return 0;}
-
-inline int funcc0(double);
-inline int funcc1(double) {return 1;}
+// OBJECT  GLOBAL DEFAULT
+int d0;
+// OBJECT  GLOBAL DEFAULT
+int d1 = 0;
+// OBJECT  GLOBAL DEFAULT
+int d2 = 3;
--- Linkage/symbol/binding/binding2.cpp
+++ Linkage/symbol/binding/binding2.cpp
@@ -1,10 +1,18 @@
 // $ g++ -c binding2.cpp -o binding2.o && readelf --symbols binding2.o | c++filt > dump
 
 // FUNC    LOCAL  DEFAULT
-static int local(void) { }
+static int local(void) { return 0; }
+static int local2(void);
 
 // FUNC    GLOBAL DEFAULT
-int global(void) { }
+int global(void) { return 0; }
+int global2(void);
 
 // FUNC    WEAK   DEFAULT
-int  __attribute__((weak)) weak(void) { }
+int  __attribute__((weak)) weak(void) { return 0; }
+int  __attribute__((weak)) weak2(void);
+
+inline int none(void) { return 0; }
+inline int none2(void);
+
+extern int ext(void);
