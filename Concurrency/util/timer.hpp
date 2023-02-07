4e47b247d8b81ae5cf9c3a4e1194e867b0f8ed67
Caturra <wasdjkl233@gmail.com>
2023-02-07 13:22:43 +0800

    update timer

--- Concurrency/util/timer.hpp
+++ Concurrency/util/timer.hpp
@@ -1,17 +1,17 @@
 #pragma once
 #include <bits/stdc++.h>
 
-struct {
+inline struct {
     std::chrono::steady_clock::time_point clock_start, clock_end;
 } global;
 
 [[gnu::constructor]]
-void global_start() {
+inline void global_start() {
     global.clock_start = std::chrono::steady_clock::now();
 }
 
 [[gnu::destructor]]
-void global_end() {
+inline void global_end() {
     global.clock_end = std::chrono::steady_clock::now();
     using ToMilli = std::chrono::duration<double, std::milli>;
     auto elapsed = ToMilli{global.clock_end - global.clock_start}.count();
