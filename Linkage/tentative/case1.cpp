a9ba1677b3dc37d886efc7155a37dc402e224af9
Caturra <wasdjkl233@gmail.com>
2023-02-17 15:36:13 +0800

    add tentative

---
+++ Linkage/tentative/case1.cpp
@@ -0,0 +1,6 @@
+// g++ -x c -c -fcommon case1.cpp -o case1_c.o && objdump -t case1_c.o > case1_c_dump
+// g++ -c -fcommon case1.cpp -o case1_cpp.o && objdump -t case1_cpp.o > case1_cpp_dump
+
+int a = 0;
+int b;
+
