a6a5922912c15b05026f9485c53349b17b9e288e
Caturra <wasdjkl233@gmail.com>
2023-02-22 21:39:53 +0800

    add relocate

---
+++ Linkage/relocate/case1.cpp
@@ -0,0 +1,15 @@
+static int static_var;
+extern int extern_var;
+int global_var = 6;
+
+static void static_func() {}
+extern void extern_func();
+
+void test_relocate() {
+    static_var = 1;
+    extern_var = 2;
+    global_var = 3;
+
+    static_func();
+    extern_func();
+}
