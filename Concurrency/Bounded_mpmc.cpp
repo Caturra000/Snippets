351d7ab8db1039a375246e0ec2e8b7a3c4fd54c7
Caturra <wasdjkl233@gmail.com>
2023-11-15 02:39:26 +0800

    update bounded_mpmc: add try functions

--- Concurrency/Bounded_mpmc.cpp
+++ Concurrency/Bounded_mpmc.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 
-// This class is not for generic usage.
+// This toy class is NOT for generic usage.
 // SIZE must be a power of 2.
 template <typename T, size_t SIZE>
 struct Bounded_mpmc {
@@ -33,6 +33,42 @@ struct Bounded_mpmc {
         slot.seq.store(seq(out) + 2, std::memory_order_release);
         return val;
     }
+
+    bool try_push(T elem) {
+        size_t in = _in.load(std::memory_order_acquire);
+        for(;;) {
+            auto &slot = _buf[idx(in)];
+            if(seq(in) != slot.seq.load(std::memory_order_acquire)) {
+                size_t old_in = in;
+                // Advance and update local `in`.
+                in = _in.load(std::memory_order_acquire);
+                if(old_in == in) return false;
+            } else if(_in.compare_exchange_weak(in, in+1,
+                        std::memory_order_acq_rel, std::memory_order_relaxed)) {
+                slot.val = std::move(elem);
+                slot.seq.store(seq(in) + 1, std::memory_order_release);
+                return true;
+            }
+        }
+    }
+
+    std::optional<T> try_pop() {
+        size_t out = _out.load(std::memory_order_acquire);
+        for(;;) {
+            auto &slot = _buf[idx(out)];
+            if(seq(out) + 1 != slot.seq.load(std::memory_order_acquire)) {
+                size_t old_out = out;
+                out = _out.load(std::memory_order_acquire);
+                if(old_out == out) return std::nullopt;
+            } else if(_out.compare_exchange_weak(out, out+1,
+                        std::memory_order_acq_rel, std::memory_order_relaxed)) {
+                auto opt = std::make_optional<T>(std::move(slot.val));
+                slot.seq.store(seq(out) + 2, std::memory_order_release);
+                return opt;
+            }
+        }
+    }
+
 };
 
 int main() {
@@ -42,20 +78,34 @@ int main() {
     constexpr size_t count = 1e8;
     constexpr size_t producer_size = 10;
     constexpr size_t consumer_size = 10;
-    static_assert(count % producer_size == 0);
-    static_assert(count % consumer_size == 0);
+    static_assert(producer_size > 1 && count % producer_size == 0);
+    static_assert(consumer_size > 1 && count % consumer_size == 0);
+    struct Property {};
+    constexpr struct Blocking: Property {} blocking;
+    constexpr struct Non_blocking: Property {} non_blocking;
     std::atomic<size_t> g_value = 0;
     using namespace std::views;
-    auto produce = [&](size_t first_value, size_t partial_count) {
+    auto produce = [&]<std::derived_from<Property> T>(size_t first_value, size_t partial_count, T) {
         for(auto c : iota(first_value) | take(partial_count)) {
-            mpmc->push(c);
+            if constexpr (std::is_same_v<T, Blocking>) {
+                mpmc->push(c);
+            } else {
+                while(!mpmc->try_push(c));
+            }
         }
     };
-    auto consume = [&](int partial_count) {
+    auto consume = [&]<std::derived_from<Property> T>(int partial_count, T) {
         size_t sum = 0;
         for(auto c : iota(0, partial_count)) {
-            auto val = mpmc->pop();
-            sum += val;
+            std::ignore = c;
+            if constexpr (std::is_same_v<T, Blocking>) {
+                auto val = mpmc->pop();
+                sum += val;
+            } else {
+                std::optional<size_t> opt;
+                while(!(opt = mpmc->try_pop()));
+                sum += *opt;
+            }
         }
         g_value += sum;
     };
@@ -66,14 +116,21 @@ int main() {
 
         size_t producer_first_value = 1;
         constexpr size_t producer_take = count / producer_size;
+        bool rr = false;
         for(auto &&p : producers) {
-            p = std::jthread{produce, producer_first_value, producer_take};
+            auto make_thread = [&](auto blocking_property) {
+                return std::jthread{produce, producer_first_value, producer_take, blocking_property};
+            };
+            p = (rr ^= 1) ? make_thread(blocking) : make_thread(non_blocking);
             producer_first_value += producer_take;
         }
 
         constexpr size_t consumer_take = count / consumer_size;
         for(auto &&c : consumers) {
-            c = std::jthread{consume, consumer_take};
+            auto make_thread = [&](auto blocking_property) {
+                return std::jthread{consume, consumer_take, blocking_property};
+            };
+            c = (rr ^= 1) ? make_thread(blocking) : make_thread(non_blocking);
         }
     }
 
