c7bda832bfac83730babe1843ef11f0b5f5d8845
Caturra <wasdjkl233@gmail.com>
2023-12-08 22:49:53 +0800

    update syscall_assembly: none.s

---
+++ perf/syscall_assembly/none.s
@@ -0,0 +1,18 @@
+.text
+   .global _start
+_start:
+   movq $100000000, %rcx
+TEST:
+   pushq %rcx
+   movq $4, %rax    # sys_write
+   movq $-1, %rbx
+   movq $0, %rcx
+   movq $0, %rdx
+
+   # None.
+
+   popq %rcx
+   loop TEST
+   movq $1, %rax    # sys_exit
+   xorq %rbx, %rbx
+   int $0x80
