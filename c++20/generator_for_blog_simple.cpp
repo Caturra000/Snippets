7f81b0fc9d529c628bd538d02b4eb92a7117cd73
Caturra <wasdjkl233@gmail.com>
2023-12-20 01:57:48 +0800

    update generator: for blog

---
+++ c++20/generator_for_blog_simple.cpp
@@ -0,0 +1,87 @@
+#include <coroutine>
+#include <iostream>
+#include <ranges>
+
+template <typename T>
+class My_generator {
+public:
+    struct promise_type;
+
+    // ctor / dtor
+    explicit constexpr
+      My_generator(std::coroutine_handle<promise_type> h) noexcept
+        : _handle(h) {}
+    ~My_generator() { _handle ? _handle.destroy() : void(0); }
+
+    // copy / move
+    My_generator(const My_generator &) = delete;
+    My_generator(My_generator &&rhs) = delete;
+    My_generator& operator=(const My_generator &rhs) = delete;
+
+    // user APIs
+    T operator ()() {
+        if(!resumable()) {
+            throw std::runtime_error("AIEEEEE! A NINJA!?");
+        }
+        if(!operator bool()) {
+            throw std::logic_error("WHY THERE'S A NINJA HERE!?");
+        }
+        _handle.resume();
+        _handle.promise().count--;
+        return std::move(_handle.promise().result);
+    }
+
+    explicit operator bool() const noexcept {
+        return _handle.promise().count > 0;
+    }
+
+    bool resumable() const noexcept {
+        return _handle && !_handle.done();
+    }
+
+private:
+    std::coroutine_handle<promise_type> _handle;
+};
+
+template <typename T>
+struct My_generator<T>::promise_type {
+    explicit constexpr
+      promise_type(size_t count) noexcept(noexcept(T())): count(count) {}
+    auto initial_suspend() noexcept { return std::suspend_always{}; }
+    auto final_suspend() noexcept { return std::suspend_never{}; }
+    void unhandled_exception() {}
+
+    My_generator<T> get_return_object();
+    auto yield_value(auto &&value);
+
+    size_t count;
+    T result;
+};
+
+template <typename T>
+My_generator<T> My_generator<T>::promise_type::get_return_object() {
+    auto handle = std::coroutine_handle<promise_type>::from_promise(*this);
+    return My_generator{handle};
+}
+
+template <typename T>
+auto My_generator<T>::promise_type::yield_value(auto &&value) {
+    result = std::forward<decltype(value)>(value);
+    return std::suspend_always{};
+}
+
+// C++ coroutines are special functions
+static My_generator<int> coroutine_function(size_t n) {
+    for(auto i : std::views::iota(0)) {
+        co_yield i;
+    }
+}
+
+int main() {
+    constexpr size_t n = 7;
+    auto generate = coroutine_function(n);
+    while(generate) {
+        std::cout << generate() << ' ';
+    }
+    return 0;
+}
