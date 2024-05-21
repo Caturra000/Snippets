e4af04aca51a7075c7a3eec0dd443200f1ea6ea2
Caturra <wasdjkl233@gmail.com>
2024-05-22 04:17:31 +0800

    update tcp_state

--- net/tcp_state/tcp_state.cpp
+++ net/tcp_state/tcp_state.cpp
@@ -115,81 +115,81 @@ struct LA: CW {
 
 //////////////////////////////////////////////////////////////////
 
-void make_ack(auto &base) {
-    base.os << std::format(
+void make_ack(auto &state) {
+    state.os << std::format(
         "+.1 < . {0}:{0}(0) ack {1} win {2}\n"
-    , base.in_seq, base.out_seq+1, base.win);
+    , state.in_seq, state.out_seq+1, state.win);
 }
 
-void make_data(auto &base) {
+void make_data(auto &state) {
     constexpr int len = 10;
-    base.os << std::format(
+    state.os << std::format(
         "+.1 < P. {}:{}({}) ack {} win {}\n"
-    , base.in_seq, base.in_seq+len, len, base.out_seq+1, base.win);
-    base.in_seq += len;
+    , state.in_seq, state.in_seq+len, len, state.out_seq+1, state.win);
+    state.in_seq += len;
 }
 
-void make_data_random(auto &base) {
+void make_data_random(auto &state) {
     constexpr int len = 10;
-    base.os << std::format(
+    state.os << std::format(
         "+.1 < P. {}:{}({}) ack 56789 win {}\n"
-    , base.in_seq, base.in_seq+len, len, base.win);
-    base.in_seq += len;
+    , state.in_seq, state.in_seq+len, len, state.win);
+    state.in_seq += len;
 }
 
-void make_finack(auto &base) {
-    base.os << std::format(
+void make_finack(auto &state) {
+    state.os << std::format(
         "+.1 < F. {0}:{0}(0) win {1}\n"
-    , base.in_seq, base.win);
-    base.in_seq++;
+    , state.in_seq, state.win);
+    state.in_seq++;
 }
 
-void make_syn(auto &base) {
-    base.os << std::format(
+void make_syn(auto &state) {
+    state.os << std::format(
         "+.1 < S {0}:{0}(0) win {1}\n"
-    , base.in_seq, base.win);
-    base.in_seq++;
+    , state.in_seq, state.win);
+    state.in_seq++;
 }
 
-void make_synack(auto &base) {
-    base.os << std::format(
+void make_synack(auto &state) {
+    state.os << std::format(
         "+.1 < S. {0}:{0}(0) ack {1} win {2}\n"
-    , base.in_seq, base.out_seq+1, base.win);
-    base.in_seq++;
+    , state.in_seq, state.out_seq+1, state.win);
+    state.in_seq++;
 }
 
 void make_null(auto &) {}
 
 //////////////////////////////////////////////////////////////////
 
-template <typename T>
-using Make_func_ptr_type = void(*)(T&);
-
 // rfc9293目录是未经任何修改报文，其文件名（state_names）对应于连接可达到的状态
 // 其余目录会在rfc9293的基础上使用make_前缀函数构造多余的报文
-std::string dirs[] {"rfc9293", "synack", "ack", "data", "data_random", "finack"};
-
-template <typename T>
-Make_func_ptr_type<T> make_funcs[] {
-    make_null, make_synack, make_ack, make_data, make_data_random, make_finack
+template <typename T, typename Ptr = void(*)(T&)>
+auto dir2func = std::unordered_map<std::string_view, Ptr> {
+    {"rfc9293",     make_null},
+    {"synack",      make_synack},
+    {"ack",         make_ack},
+    {"data",        make_data},
+    {"data_random", make_data_random},
+    {"finack",      make_finack}
 };
 
-std::string state_names[]     {"LISTEN", "SYN_RCVD", "SYN_SENT", "ESTAB", "FW1", "FW2", "TW", "CW", "LA"};
-using State_types = std::tuple< LISTEN,   SYN_RCVD,   SYN_SENT,   ESTAB,   FW1,   FW2,   TW,   CW,   LA>;
+std::string_view state_names[]{"LISTEN", "SYN_RCVD", "SYN_SENT", "ESTAB", "FW1", "FW2", "TW", "CW", "LA"};
+using State_types = std::tuple< LISTEN,   SYN_RCVD,   SYN_SENT,   ESTAB,   FW1,   FW2,   TW,   CW,   LA >;
 
 //////////////////////////////////////////////////////////////////
 
 int main() {
-    [&]<size_t ...Is>(std::index_sequence<Is...>) {
+    using namespace std::string_literals;
+    [enable_strcat = ""s]<size_t ...Is>(std::index_sequence<Is...>) {
         ([&]<size_t I>() {
-            using T = std::tuple_element_t<I, State_types>;
-            auto &funcs = make_funcs<T>;
-            namespace vs = std::views;
-            for(auto idx : vs::iota(0) | vs::take(std::size(funcs))) {
-                std::filesystem::create_directories(dirs[idx]);
-                std::ofstream out(dirs[idx] + "/" + state_names[I]);
-                T conn(out);
-                funcs[idx](conn);
+            using State = std::tuple_element_t<I, State_types>;
+            for(auto &&[dir, func] : dir2func<State>) {
+                std::filesystem::create_directories(dir);
+                auto path = enable_strcat + dir.data() + "/" + state_names[I].data();
+                std::ofstream filestream(path);
+                State state(filestream);
+                func(state);
             }
         }.template operator()<Is>(), ...);
     }(std::make_index_sequence<std::size(state_names)>{});
