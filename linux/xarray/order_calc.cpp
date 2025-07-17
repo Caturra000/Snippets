2ed330529f8a4e29f7356dd77215be0070f4468d
Caturra <wasdjkl233@gmail.com>
2025-07-17 18:37:43 +0800

    update xarray: add max

--- linux/xarray/order_calc.cpp
+++ linux/xarray/order_calc.cpp
@@ -12,11 +12,20 @@ auto calc(I index, O order) {
     auto xa_index = order < 64 ? (index >> order) << order : 0;
     auto xa_shift = order - (order % CHUNK_SHIFT);
     auto xa_sibs = (1 << (order % CHUNK_SHIFT)) - 1;
-    return std::tuple(xa_index, xa_shift, xa_sibs);
+    // See xas_max().
+    auto xa_max = xa_index;
+    if(xa_shift || xa_sibs) {
+        auto mask = ((xa_sibs + 1) << xa_shift) - 1;
+        xa_max |= mask;
+        if(xa_max == mask) xa_max++;
+    }
+    return std::tuple(xa_index, xa_shift, xa_sibs, xa_max);
 }
 
-auto output(auto index, auto shift, auto sibs) {
-    std::cout << "[" << index << ", " << shift << ", " << sibs << "]\n";
+auto output(auto index, auto shift, auto sibs, auto max) {
+    std::cout << "[" << index << ", " << shift << ", " << sibs << "]";
+    if(verbose) std::cout << " m: " << max;
+    std::cout << std::endl;
 }
 
 auto println(auto &&...v) {
@@ -24,13 +33,13 @@ auto println(auto &&...v) {
 }
 
 int main() {
-    auto [index, shift, sibs] = calc(0,0);
-    auto c = [&] { return [&](auto ...x) { return std::tie(index, shift, sibs) = calc(x...); }; };
-    auto o = [&] { output(index, shift, sibs); };
+    auto [index, shift, sibs, max] = calc(0,0);
+    auto c = [&] { return [&](auto ...x) { return std::tie(index, shift, sibs, max) = calc(x...); }; };
+    auto o = [&] { output(index, shift, sibs, max); };
 
     auto test = [&](std::tuple<I, I> indices, O order) {
         auto [start, end] = indices;
-        if(verbose) println("test: index=[", start, ", ", end, "), order=", order);
+        println("test: index=[", start, ", ", end, "), order=", order);
         for(auto i : std::views::iota(start, end)) {
             c()(i, order);
             o();
@@ -38,9 +47,16 @@ int main() {
         println();
     };
 
-    test({0, 18}, 3);
-    test({56, 67}, 3);
-    test({56, 67}, 6);
-    test({56, 67}, 7);
-    test({56, 67}, 8);
+    std::tuple z {0, 18}; // 0 ~ 8 ~ 16 ~
+    std::tuple i {56, 78}; // ~ 64 ~
+
+    test(z, 3);
+
+    test(i, 3);
+    test(i, 6);
+    test(i, 7);
+    test(i, 8);
+
+    test(i, 11);
+    test(i, 12);
 }
